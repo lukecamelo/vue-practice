c1e74a7d45f3b1d271fbe17c475e0706c00e5180
luke <azathothbeats@gmail.com>
2018-06-09 18:01:47 -0400

    Messing with methods

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -2,18 +2,20 @@
 
   <div class='test'>
 
+    <input type="text" v-model="title">
     <h1>{{ title }}</h1>
 
     <p v-text="user.firstName"></p>
 
     <p v-if="showName" v-text="user.lastName"></p>
-    
+
     <ul>
       <li v-for="item in items" :key='item.id'>
         {{ item.title }} {{ item.id }}
       </li>
     </ul>
 
+    <button v-on:click="greet">Say greeting</button>
   </div>
 
 </template>
@@ -34,6 +36,15 @@ export default {
         { title: 'blip', id: 2 },
         { title: 'bebebe', id: 3 }
       ]
+    }
+  },
+  methods: {
+    greet: function() {
+      if(this.user.firstName === 'Luke') {
+        this.user.firstName = 'Billy'
+      } else {
+        this.user.firstName = 'Luke'
+      }
     }
   }
 }
