9709a63fedd66b03b5ec7fcfede8f6067c2ebbb1
luke <azathothbeats@gmail.com>
2018-06-09 18:51:13 -0400

    added basic user addition functionality

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,13 +1,23 @@
 <template>
+
   <div class='users'>
+
     <h1>{{ testmsg }}</h1>
+
+    <form v-on:submit='addUser'>
+      <input type="text" v-model='newUser.name' placeholder='Enter name'><br>
+      <input type="text" v-model='newUser.email' placeholder='Enter email'><br>
+      <input type="submit" value="submit">
+    </form>
 
     <ul>
       <li v-for='user in users' :key='user.id'>
         {{ user.name }}: {{ user.email }}
       </li>
     </ul>
+
   </div>
+
 </template>
 
 <script>
@@ -16,6 +26,9 @@ export default {
   data() {
     return {
       testmsg: 'This is the users component.',
+      newUser: {
+
+      },
       users: [
         { id: 0,name: 'Rediscover', contacted: false, email: 'rere@gmail.com' },
         { id: 1,name: 'Ken', contacted: false, email: 'kenk@gmail.com' },
@@ -24,7 +37,10 @@ export default {
     }
   },
   methods: {
-
+    addUser: function(e) {
+      e.preventDefault()
+      this.users.push({ name: this.newUser.name, email: this.newUser.email })
+    }
   }  
 }
 </script>
